617157c09c4b1bfdabf45f0a82340ee0c63fe419
jiyinyiyong <jiyinyiyong@gmail.com>
2017-12-02 19:16:09 +0800

    refactoring to simplified layout

--- src/cirru_writer/comp/container.cljs
+++ src/cirru_writer/comp/container.cljs
@@ -1,23 +1,36 @@
 
 (ns cirru-writer.comp.container
-  (:require [respo.macros :refer [defcomp div <> textarea button]]
+  (:require [respo.macros :refer [defcomp div <> textarea button pre]]
             [respo-ui.style :as ui]
             [hsl.core :refer [hsl]]
-            [respo.comp.space :refer [=<]]))
+            [respo.comp.space :refer [=<]]
+            [cljs.reader :refer [read-string]]
+            [cirru-writer.core :refer [emit-string generate-statements]]))
+
+(def style-code
+  {:font-family "Menlo,monospace",
+   :background-color (hsl 0 0 94),
+   :padding 8,
+   :font-size 12,
+   :overflow :auto,
+   :white-space :pre-line,
+   :line-height 1.8})
 
 (defcomp
  comp-container
  (store)
  (div
-  {:style ui/row}
+  {:style (merge ui/row ui/fullscreen)}
   (textarea
-   {:style (merge ui/textarea {:width 400, :height 600, :font-family "Menlo,monospace"}),
+   {:style (merge
+            ui/textarea
+            {:width 400, :flex-shrink 0, :height 600, :font-family "Menlo,monospace"}),
     :value (:content store),
     :on {:input (fn [e d! m!] (d! :content (:value e)))}})
   (=< 16 nil)
-  (div
-   {}
-   (button
-    {:style ui/button, :on {:click (fn [e d! m!] (println (:content store)))}}
-    (<> "click"))
-   (<> (:content store)))))
+  (let [ops (generate-statements (read-string (:content store))), result (emit-string ops)]
+    (println "result" result)
+    (div
+     {}
+     (pre {:style style-code} (<> ops))
+     (pre {:style (merge style-code {:white-space :pre})} (<> result))))))
--- src/cirru_writer/core.cljs
+++ src/cirru_writer/core.cljs
@@ -1,182 +1,77 @@
 
 (ns cirru-writer.core (:require [clojure.string :as string]))
 
-(declare write-inline-node)
+(defn boxed? [expr] (every? vector? expr))
 
-(declare write-in-between-node)
+(defn simple? [expr] (and (vector? expr) (every? string? expr)))
 
-(declare write-inline-line)
+(defn generate-leaf [leaf] leaf)
 
-(declare write-block)
+(defn generate-inline-expr [expr]
+  [:open
+   (loop [result [], nodes expr, head? true]
+     (if (empty? nodes)
+       result
+       (let [next-child (let [cursor (first nodes)
+                              child-form (if (string? cursor)
+                                           (generate-leaf cursor)
+                                           (generate-inline-expr cursor))]
+                          (if head? child-form [:space child-form]))
+             next-result (if (empty? result) next-child [result next-child])]
+         (recur next-result (rest nodes) false))))
+   :close])
 
-(declare write-last-node)
+(defn generate-tree [expr insist-head?]
+  (loop [acc [], exprs expr, head? true, prev-kind nil]
+    (if (empty? exprs)
+      acc
+      (let [cursor (first exprs)
+            kind (cond
+                   (string? cursor) :leaf
+                   (simple? cursor) :simple-expr
+                   (boxed? cursor) :boxed-expr
+                   :else :expr)
+            child (if (= kind :leaf)
+                    (generate-leaf cursor)
+                    (if (and head? insist-head?)
+                      (generate-inline-expr cursor)
+                      (case kind
+                        :simple-expr
+                          (if (= prev-kind :leaf)
+                            (generate-inline-expr cursor)
+                            [:indent :newline (generate-tree cursor false) :unindent])
+                        :expr [:indent :newline (generate-tree cursor false) :unindent]
+                        :boxed-expr
+                          [:indent (generate-tree cursor (= prev-kind :boxed-expr)) :unindent]
+                        nil)))
+            result (if (or (and (= prev-kind :leaf) (contains? #{:leaf :simple-expr} kind))
+                           (and (contains? #{:leaf :simple-expr} prev-kind) (= kind :leaf)))
+                     [:space child]
+                     child)]
+        (recur (if (empty? acc) result [acc result]) (rest exprs) false kind)))))
 
-(declare write-inline-expression)
+(defn render-spaces
+  ([n] (render-spaces "" n))
+  ([acc n] (if (zero? n) acc (recur (str acc "  ") (dec n)))))
 
-(defn control-inline [state] (assoc state :inline? true))
+(defn emit-string [operations]
+  (loop [acc "", ops (flatten operations), level 0]
+    (if (empty? ops)
+      acc
+      (let [op (first ops)
+            piece (cond
+                    (string? op) op
+                    (= op :newline) (str "\n" (render-spaces level))
+                    (= op :open) "("
+                    (= op :close) ")"
+                    (= op :space) " "
+                    (= op :indent) ""
+                    (= op :unindent) ""
+                    :else {:type :unknown, :data op})
+            next-level (case op :indent (inc level) :unindent (dec level) level)]
+        (recur (str acc piece) (rest ops) next-level)))))
 
-(defn write-raw [tree state] (update-in state [:code] (fn [code] (str code tree))))
+(defn generate-statements [exprs]
+  (mapv (fn [xs] [:newline (generate-tree xs true) :newline]) exprs))
 
-(defn put-left-paren [state] (write-raw "(" state))
-
-(defn write-string [tree state]
-  (update-in state [:code] (fn [code] (str code (pr-str tree)))))
-
-(defn put-space [state] (write-raw " " state))
-
-(defn write-token [tree state]
-  (if (re-find (re-pattern "[\\(\\)\\ \\\"]") tree)
-    (write-string tree state)
-    (write-raw tree state)))
-
-(defn put-right-paren [state] (write-raw ")" state))
-
-(defn write-inline-expression [tree state]
-  (->> state (put-left-paren) (write-inline-line tree) (put-right-paren)))
-
-(defn write-inline-line [tree state]
-  (if (= (count tree) 0)
-    state
-    (reduce
-     (fn [acc branch] (->> acc (put-space) (write-inline-node branch)))
-     (write-inline-node (first tree) state)
-     (rest tree))))
-
-(defn write-inline-node [tree state]
-  (if (string? tree) (write-token tree state) (write-inline-expression tree state)))
-
-(defn control-outline [state] (assoc state :inline? false))
-
-(defn control-take-chance [state] (assoc state :chance-used? true))
-
-(defn control-dollar [new-value state] (assoc state :dollar-ok? new-value))
-
-(defn nested? [tree]
-  (and (vector? tree)
-       (not (empty? tree))
-       (every? #(and (vector? %) (string? (first %))) tree)))
-
-(defn control-give-chance [state] (assoc state :chance-used? false))
-
-(defn put-newline [state]
-  (update
-   state
-   :code
-   (fn [code] (str code "\n" (string/join "" (repeat (* 2 (:indentations state)) " "))))))
-
-(defn control-indent [state] (update-in state [:indentations] inc))
-
-(defn control-unindent [state] (update-in state [:indentations] dec))
-
-(defn wrap-unindent [then-call state]
-  (->> state (control-unindent) (then-call) (control-indent)))
-
-(defn put-comma [state] (write-raw "," state))
-
-(defn put-dollar [state] (write-raw "$" state))
-
-(defn write-first-node [tree state] (write-inline-node tree state))
-
-(defn wrap-indent [then-call state]
-  (->> state (control-indent) (then-call) (control-unindent)))
-
-(defn control-after-vector [new-value state] (assoc state :after-vector? new-value))
-
-(defn is-simple-expression [tree] (every? string? tree))
-
-(defn write-last-node [tree state]
-  (if (and (not (:after-vector? state)) (:dollar-ok? state))
-    (if (string? tree)
-      (->> state (put-space) (write-token tree))
-      (if (> (count tree) 0)
-        (->> state
-             (put-space)
-             (wrap-unindent
-              (fn [inner-state]
-                (->> inner-state
-                     (put-dollar)
-                     (put-space)
-                     (control-dollar false)
-                     (control-inline)
-                     (control-give-chance)
-                     (write-block tree)
-                     (control-outline)))))
-        (->> state (put-space) (write-raw "()"))))
-    (->> state (control-dollar true) (write-in-between-node tree))))
-
-(defn write-block [tree state]
-  (case (count tree)
-    0 (throw (js/Error. "nothing to write as a line"))
-    1 (write-first-node (first tree) state)
-    (let [head (first tree), between (rest (butlast tree)), tail (last tree)]
-      (->> state
-           (write-first-node head)
-           (control-inline)
-           (control-after-vector false)
-           (wrap-indent
-            (fn [inner-state]
-              (as->
-               inner-state
-               ss
-               (reduce
-                (fn [acc branch]
-                  (->> acc (control-dollar (:dollar-ok? ss)) (write-in-between-node branch)))
-                ss
-                between)
-               (write-last-node tail ss))))
-           (control-outline)))))
-
-(defn write-in-between-node [tree state]
-  (if (string? tree)
-    (if (:inline? state)
-      (->> state (put-space) (write-token tree))
-      (->> state
-           (put-newline)
-           (control-inline)
-           (put-comma)
-           (control-give-chance)
-           (put-space)
-           (write-token tree)
-           (control-after-vector true)))
-    (cond
-      (and (not (:chance-used? state))
-           (not (:after-vector? state))
-           (is-simple-expression tree))
-        (->> state
-             (put-space)
-             (write-inline-expression tree)
-             (control-take-chance)
-             (control-after-vector true))
-      :else
-        (->> state
-             (put-newline)
-             (control-inline)
-             (control-give-chance)
-             (write-block tree)
-             (control-outline)
-             (control-after-vector true)))))
-
-(def initial-state
-  {:code "",
-   :indentations 0,
-   :inline? false,
-   :chance-used? false,
-   :dollar-ok? true,
-   :after-vector? false})
-
-(defn write-program [tree state]
-  (reduce
-   (fn [acc branch]
-     (->> acc
-          (put-newline)
-          (control-inline)
-          (control-give-chance)
-          (control-after-vector false)
-          (control-dollar true)
-          (write-block branch)
-          (control-outline)
-          (put-newline)))
-   state
-   tree))
-
-(defn write [tree] (:code (write-program tree initial-state)))
+(defn write-code [exprs] (emit-string (generate-statements exprs)))
